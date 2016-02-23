b74990587202d66b6923cbfabebdaca4a0f9cdc3
umberto <umberto.degiovannini@gmail.com>
2016-02-23 17:56:55 +0100

    always update the first 6 iterations

--- src/hamiltonian/pcm.F90
+++ src/hamiltonian/pcm.F90
@@ -1894,7 +1894,7 @@ contains
       FLOAT,       intent(in)    :: time
 
       this%iter = this%iter + 1 
-      update = (this%iter == 1 .or. mod(this%iter, this%update_iter) == 0)
+      update = (this%iter <= 6 .or. mod(this%iter, this%update_iter) == 0)
       
       if (debug%info .and. update) then
         call messages_write(' PCM potential updated')
