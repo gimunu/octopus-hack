09282b4fc858b876322fbcbeab5eae16de7e076f
Umberto De Giovannini <umbe@eos02.bc.rzg.mpg.de>
2016-06-08 12:41:17 +0200

    fixed race conditions with resatrt write

--- src/td/pes_flux_out_inc.F90
+++ src/td/pes_flux_out_inc.F90
@@ -1212,11 +1212,21 @@ subroutine pes_flux_dump(restart, this, mesh, st, ierr)
     if(this%shape == M_PLANES) then
       write(filename,'(i5.5)') ik
       write(filename,'(a)') trim(restart_dir(restart))//"/pesflux4-kpt"//trim(filename)//".obf"
-      
+ 
+
+if(mpi_grp_is_root(st%mpi_grp)) then      
       SAFE_ALLOCATE(psi1(this%nkpnts))
       psi1(:)=this%conjgphase_prev_cub(:,ik)
       call io_binary_write(filename, this%nkpnts, psi1(:), err)
       SAFE_DEALLOCATE_P(psi1)
+end if
+
+#if defined(HAVE_MPI)
+  if(st%mpi_grp%size > 1) then
+    call MPI_Bcast(err, 1, MPI_INTEGER, 0, st%mpi_grp%comm, mpi_err)
+    call MPI_Barrier(st%mpi_grp%comm, mpi_err)
+  end if
+#endif
 
       if (err /= 0) then
         message(1) = "Unable to write restart information to '"//trim(filename)//"'."
